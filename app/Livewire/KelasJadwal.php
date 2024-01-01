93980e7c177481159355016c6a24b6fc7fc0a6ea
fadli raditya <fadliraditya2004@gmail.com>
2024-01-02 00:52:28 +0700

    home

--- app/Livewire/KelasJadwal.php
+++ app/Livewire/KelasJadwal.php
@@ -22,7 +22,7 @@ class KelasJadwal extends Component {
     }
     public function closeModal() {
         $this->isModal = true;
-        return redirect('/workout');
+        return redirect('/');
     }
 
     public function openModal() {
